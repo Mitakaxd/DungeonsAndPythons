b0c37b6ab2363db20ed7ffd523022a5509c35911
Mitakaxd <mitaka.g.g@abv.bg>
2019-04-10 17:07:46 +0300

    added properties for spells and weapons

--- abilities.py
+++ abilities.py
@@ -1,7 +1,16 @@
-class AbilityMixin:
+class AbilityMixin: 
     def __init__(self, name, damage):
         self._name = name
         self._damage = dmg
+
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def damage(self):
+        return self._damage
+    
 class Weapon(AbilityMixin):
     def __init__(self, name, damage):
         super().__init__(name,damage)
@@ -11,3 +20,16 @@ class Spell(AbilityMixin):
         super().__init__(name,damage)
         self._mana_cost = mana_cost
         self._cast_range = cast_range
+    
+    @property   
+    def mana_cost(self):
+        return self._mana_cost
+    
+    @property
+    def cast_range(self):
+        return self._cast_range
+    
+
+@property
+def cast_range(self):
+    return self._cast_range
